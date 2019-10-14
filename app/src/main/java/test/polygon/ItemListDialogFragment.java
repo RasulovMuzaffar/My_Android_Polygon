d44549e8a8c97522befb44a2c22521d8fc0473f7
Muzaffar <rasulovmuzaffar@gmail.com>
2019-10-14 08:35:11 +0500

    aaa

--- app/src/main/java/test/polygon/ItemListDialogFragment.java
+++ app/src/main/java/test/polygon/ItemListDialogFragment.java
@@ -1,18 +1,15 @@
 package test.polygon;
 
 import android.app.Dialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.StyleRes;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
@@ -21,7 +18,6 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 
 public class ItemListDialogFragment extends BottomSheetDialogFragment {
-
 
     @Nullable
     @Override
@@ -37,14 +33,51 @@ public class ItemListDialogFragment extends BottomSheetDialogFragment {
 
     @Override
     public void setupDialog(@NonNull Dialog dialog, int style) {
-        View v = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_item_list_dialog, null);
-        dialog.setContentView(v);
+//        View v = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_item_list_dialog, null);
+//        dialog.setContentView(v);
 
 //        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) ((View) v.getParent()).getLayoutParams();
 //        CoordinatorLayout.Behavior behavior = params.getBehavior();
 //        ((BottomSheetBehavior) behavior).setState(BottomSheetBehavior.STATE_EXPANDED);
 
-        BottomSheetBehavior.from(v).setPeekHeight(Resources.getSystem().getDisplayMetrics().heightPixels);
-        BottomSheetBehavior.from(v).setState(BottomSheetBehavior.STATE_EXPANDED);
+//        BottomSheetDialog sheetDialog = (BottomSheetDialog) dialog;
+//        sheetDialog.findViewById(com.google.android.material.R.id.design_bottom_sheet);
+//
+//        BottomSheetBehavior behavior=BottomSheetBehavior.from(sheetDialog.findViewById(com.google.android.material.R.id.design_bottom_sheet));
+//        behavior.setPeekHeight(Resources.getSystem().getDisplayMetrics().heightPixels);
+//        behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+
+
+//        BottomSheetBehavior.from(v).setPeekHeight(Resources.getSystem().getDisplayMetrics().heightPixels);
+//        BottomSheetBehavior.from(v).setState(BottomSheetBehavior.STATE_EXPANDED);
+
+
+        super.setupDialog(dialog, style);
+        View inflatedView = View.inflate(getContext(), R.layout.fragment_item_list_dialog, null);
+        dialog.setContentView(inflatedView);
+
+
+        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) ((View) inflatedView.getParent()).getLayoutParams();
+        CoordinatorLayout.Behavior behavior = params.getBehavior();
+
+//        if (behavior != null && behavior instanceof BottomSheetBehavior) {
+//            ((BottomSheetBehavior) behavior).setBottomSheetCallback(mBottomSheetBehaviorCallback);
+//        }
+
+        View parent = (View) inflatedView.getParent();
+        parent.setFitsSystemWindows(true);
+        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(parent);
+        inflatedView.measure(0, 0);
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int screenHeight = displaymetrics.heightPixels;
+        bottomSheetBehavior.setPeekHeight(screenHeight);
+
+//        if (params.getBehavior() instanceof BottomSheetBehavior) {
+//            ((BottomSheetBehavior) params.getBehavior()).setBottomSheetCallback(mBottomSheetBehaviorCallback);
+//        }
+
+        params.height = screenHeight;
+        parent.setLayoutParams(params);
     }
 }
